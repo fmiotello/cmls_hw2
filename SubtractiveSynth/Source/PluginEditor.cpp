824bb1c8d2be5a52fbcda600be291b707e2ded5a
Jameau Clement <clemjam@hotmail.fr>
2020-05-01 19:19:38 +0200

    first shot for the GUI, working the flexbox

--- SubtractiveSynth/Source/PluginEditor.cpp
+++ SubtractiveSynth/Source/PluginEditor.cpp
@@ -15,9 +15,23 @@
 SubtractiveSynthAudioProcessorEditor::SubtractiveSynthAudioProcessorEditor (SubtractiveSynthAudioProcessor& p)
     : AudioProcessorEditor (&p), processor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+
+    setSize (400, 250);
+
+    waveformLabel.setText("Waveform :", dontSendNotification);
+    filterLabel.setText("Filter(?) :", dontSendNotification);
+    waveform.setRange(1, 5, 1);
+    waveform.setSliderStyle(Slider::Rotary);
+    waveform.setTextBoxStyle(Slider::TextBoxBelow, false, 100, 20);
+    waveform.addListener(this);
+    filter.setRange(0, 2, 1);
+    filter.setSliderStyle(Slider::Rotary);
+    filter.setTextBoxStyle(Slider::TextBoxBelow, false, 100, 20);
+    filter.addListener(this);
+    addAndMakeVisible(waveform);
+    addAndMakeVisible(waveformLabel);
+    addAndMakeVisible(filter);
+    addAndMakeVisible(filterLabel);
 }
 
 SubtractiveSynthAudioProcessorEditor::~SubtractiveSynthAudioProcessorEditor()
@@ -32,11 +46,48 @@ void SubtractiveSynthAudioProcessorEditor::paint (Graphics& g)
 
     g.setColour (Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), Justification::centred, 1);
+    //g.drawFittedText ("Hello World!", getLocalBounds(), Justification::centred, 1);
 }
 
 void SubtractiveSynthAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    Rectangle<int> bounds = getLocalBounds();
+
+    FlexBox mainFlexBox;
+
+    //FlexItem fb1(getWidth() / 2.0f, getHeight());
+    //FlexItem fb2(getWidth() / 2.0f, getHeight());         // To have just 2 cells, see that later
+
+    mainFlexBox.flexDirection = FlexBox::Direction::row;
+    mainFlexBox.flexWrap = FlexBox::Wrap::wrap;
+    mainFlexBox.justifyContent = FlexBox::JustifyContent::spaceAround;
+    mainFlexBox.alignContent = FlexBox::AlignContent::center;
+
+    Array<FlexItem> mainItemArray;
+
+    mainItemArray.add(FlexItem(65, 50, waveformLabel));
+    mainItemArray.add(FlexItem(100, 100, waveform));
+    mainItemArray.add(FlexItem(65, 50, filterLabel));
+    mainItemArray.add(FlexItem(100, 100, filter));
+
+    mainFlexBox.items = mainItemArray;
+
+    mainFlexBox.performLayout(bounds);
+
+
+    //waveformLabel.setBounds(30, 50, 130, 20);
+    //waveform.setBounds(30, 80, 100, 100);
+    //filterLabel.setBounds(200, 50, 130, 20);
+    //filter.setBounds(200, 80, 100, 100);
+}
+
+void SubtractiveSynthAudioProcessorEditor::sliderValueChanged(Slider * slider) {
+    if (slider == &waveform) {
+        //processor.setFreq(freq.getValue());
+        DBG("dbg : Waveform Changed");
+    }
+    else if (slider == &filter) {
+        DBG("dbg : Filter Changed");
+        //processor.setAmplitude(amplitude.getValue());
+    }
 }
--- SubtractiveSynth/Source/PluginEditor.h
+++ SubtractiveSynth/Source/PluginEditor.h
@@ -16,7 +16,7 @@
 //==============================================================================
 /**
 */
-class SubtractiveSynthAudioProcessorEditor  : public AudioProcessorEditor
+class SubtractiveSynthAudioProcessorEditor  : public AudioProcessorEditor, private Slider::Listener
 {
 public:
     SubtractiveSynthAudioProcessorEditor (SubtractiveSynthAudioProcessor&);
@@ -30,6 +30,13 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SubtractiveSynthAudioProcessor& processor;
+
+    Slider waveform;
+    Label waveformLabel;
+    Slider filter;
+    Label filterLabel;
+
+    void sliderValueChanged(Slider* slider) override;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SubtractiveSynthAudioProcessorEditor)
 };
